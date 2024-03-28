c3842da494bc2b1d7684b67b529be30aff410539
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 04:45:29 +0530

    File Added

--- ch1/src/App.jsx
+++ ch1/src/App.jsx
@@ -3,7 +3,8 @@ import Video from "./components/Video";
 function App() {
   return <div>
     <div>Hello</div>
-    <Video/>
+    <Video url={"https://placebear.com/640/360"}/>
+    <Video url={"https://picsum.photos/640/360"}/>
   </div>
 }
 
