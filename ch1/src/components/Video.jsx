2675f15481a05a17f45c0f9f901f0806004d8a02
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 04:45:33 +0530

    File Added

--- ch1/src/components/Video.jsx
+++ ch1/src/components/Video.jsx
@@ -1,5 +1,12 @@
-function Video(){
-  return <div>Video</div>
+function Video({url}) {
+  let topic="React JS"
+  let bg='dark'
+  return (
+    <div>
+      <img src={url} alt="Katherine Johnson" />
+      <div className={bg} style={{backgroundColor:'red', width:'20vw', margin:'10px'}}>Tutorial Components</div>
+    </div>
+  );
 }
 
-export default Video
+export default Video;
