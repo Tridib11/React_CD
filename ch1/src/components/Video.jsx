accd912298ecbd084f6bf31b6a1ce3c6e0c3f3d4
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 16:16:35 +0530

    File Added

--- ch1/src/components/Video.jsx
+++ ch1/src/components/Video.jsx
@@ -1,5 +1,6 @@
 import "./Video.css"
 function Video({ url, title, channel="default-name", views, time }) {
+  let verified=true;
   return (
     <>
       <div className="container">
@@ -7,7 +8,7 @@ function Video({ url, title, channel="default-name", views, time }) {
           <img src={url} alt="Katherine Johnson" />
         </div>
         <div className="title">{title}</div>
-        <div className="channel">{channel}</div>
+        <div className="channel">{channel} ✅</div>
         <div className="views">
           {views } views <span>.</span> {time}
         </div>
