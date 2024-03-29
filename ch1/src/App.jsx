182a10067865b497654ecfc497feed0c12dae078
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 05:34:06 +0530

    File Added

--- ch1/src/App.jsx
+++ ch1/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <div>Videos</div>
-      <Video url={"https://placebear.com/640/360"} title={"React js Tutorial"} />
-      <Video url={"https://placebear.com/640/360"} title={"Node js Tutorial"} />
+      <Video url={"https://placebear.com/640/360"} views={"10K"} time={"1 year ago"} title={"React js Tutorial"} channel={"Tridib"} />
+      <Video url={"https://placebear.com/640/360"} views={"100K"} time={"1 month ago"} title={"Node js Tutorial"} channel={"Tridib"}/>
     </div>
   );
 }
