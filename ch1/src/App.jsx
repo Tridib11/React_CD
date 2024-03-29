54563a73c310a795283d7828755985147b3da720
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 05:31:27 +0530

    File Added

--- ch1/src/App.jsx
+++ ch1/src/App.jsx
@@ -1,11 +1,13 @@
 import Video from "./components/Video";
-
+import "./App.css";
 function App() {
-  return <div>
-    <div>Hello</div>
-    <Video url={"https://placebear.com/640/360"} topic={"bear"}/>
-    <Video url={"https://picsum.photos/640/360"} topic={"Some random pic"}/>
-  </div>
+  return (
+    <div className="App">
+      <div>Videos</div>
+      <Video url={"https://placebear.com/640/360"} title={"React js Tutorial"} />
+      <Video url={"https://placebear.com/640/360"} title={"Node js Tutorial"} />
+    </div>
+  );
 }
 
 export default App;
