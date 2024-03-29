a89341a4bbc3555d705ef3cb84e80ab92801754c
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 23:01:46 +0530

    File Added

--- ch1/src/App.jsx
+++ ch1/src/App.jsx
@@ -1,35 +1,48 @@
 import Video from "./components/Video";
 import "./App.css";
 function App() {
-  let obj={
-        url:"https://picsum.photos/id/1/160/90",
-        views:"10K",
-        time:"1 year ago",
-        title:"React js Tutorial",
-        channel:"Tridib",
-        verified:true
-  }
+  let videos = [
+    {
+      id: 1,
+      url: "https://picsum.photos/id/1/160/90",
+      views: "10K",
+      time: "1 year ago",
+      title: "React js Tutorial",
+      channel: "Tridib",
+      verified: true,
+    },
+    {
+      id: 2,
+      views: "10K",
+      time: "1 year ago",
+      title: "Node js Tutorial",
+      channel: "Tridib",
+      verified: false,
+    },
+    {
+      id: 3,
+      views: "10K",
+      time: "1 year ago",
+      title: "MongoDb Tutorial",
+      channel: "Tridib",
+      verified: true,
+    },
+  ];
   return (
     <div className="App">
       <div className="Heading">Videos</div>
       {/* passing by object destructuring */}
-      <Video {...obj}/>  
-      <Video
-        url={"https://picsum.photos/id/2/160/90"}
-        views={"100K"}
-        time={"1 month ago"}
-        title={"Node js Tutorial"}
-        channel={"Tridib"}
-        verified={true}
-      />
-       <Video
-        url={"https://picsum.photos/id/3/160/90"}
-        views={"1K"}
-        time={"1 week ago"}
-        title={"Mongo DB Tutorial"}
-        channel={"Tridib"}
-        verified={false}
-      />
+      {videos.map((video) => (
+        <Video
+          key={video.id}
+          id={video.id}
+          views={video.views}
+          time={video.time}
+          title={video.title}
+          channel={video.channel}
+          verified={video.verified}
+        />
+      ))}
     </div>
   );
 }
