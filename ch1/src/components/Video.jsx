d43a7ec5f9a477e43abf2c192acf46b021b731f3
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 23:01:48 +0530

    File Added

--- ch1/src/components/Video.jsx
+++ ch1/src/components/Video.jsx
@@ -1,10 +1,10 @@
 import "./Video.css"
-function Video({ url, title, channel="default-name", views, time,verified=false }) {
+function Video({id, title, channel="default-name", views, time,verified=false }) {
   return (
     <>
       <div className="container">
         <div className="pic">
-          <img src={url} alt="Katherine Johnson" />
+          <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
         </div>
         <div className="title">{title}</div>
         <div className="channel">{channel} {verified ? '✅' : ''}</div>
