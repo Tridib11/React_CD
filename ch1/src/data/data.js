03eee0fb1d2d9b8cace65c4c0a672464fc6923ab
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 23:05:07 +0530

    File Added

---
+++ ch1/src/data/data.js
@@ -0,0 +1,28 @@
+let videos = [
+  {
+    id: 1,
+    url: "https://picsum.photos/id/1/160/90",
+    views: "10K",
+    time: "1 year ago",
+    title: "React js Tutorial",
+    channel: "Tridib",
+    verified: true,
+  },
+  {
+    id: 2,
+    views: "10K",
+    time: "1 year ago",
+    title: "Node js Tutorial",
+    channel: "Tridib",
+    verified: false,
+  },
+  {
+    id: 3,
+    views: "10K",
+    time: "1 year ago",
+    title: "MongoDb Tutorial",
+    channel: "Tridib",
+    verified: true,
+  },
+];
+export default videos;
