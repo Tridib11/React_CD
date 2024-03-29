9d4c8b3349cbae9ed1b43369c9857878f9ff8f37
Tridib11 <tridibghosh12345@gmail.com>
2024-03-29 23:05:03 +0530

    File Added

--- ch1/src/App.jsx
+++ ch1/src/App.jsx
@@ -1,37 +1,11 @@
 import Video from "./components/Video";
 import "./App.css";
+import videos from "./data/data";
 function App() {
-  let videos = [
-    {
-      id: 1,
-      url: "https://picsum.photos/id/1/160/90",
-      views: "10K",
-      time: "1 year ago",
-      title: "React js Tutorial",
-      channel: "Tridib",
-      verified: true,
-    },
-    {
-      id: 2,
-      views: "10K",
-      time: "1 year ago",
-      title: "Node js Tutorial",
-      channel: "Tridib",
-      verified: false,
-    },
-    {
-      id: 3,
-      views: "10K",
-      time: "1 year ago",
-      title: "MongoDb Tutorial",
-      channel: "Tridib",
-      verified: true,
-    },
-  ];
+  
   return (
     <div className="App">
       <div className="Heading">Videos</div>
-      {/* passing by object destructuring */}
       {videos.map((video) => (
         <Video
           key={video.id}
