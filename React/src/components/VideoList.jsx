b02c0f7f1b089c7e90df83832f756de40f78b869
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 05:02:52 +0530

    File added

---
+++ React/src/components/VideoList.jsx
@@ -0,0 +1,31 @@
+import Video from "./Video";
+import PlayButton from "./PlayButton";
+function VideoList({videos}){
+  return (
+    <>
+    {videos.map((video) => (
+        <Video
+          key={video.id}
+          id={video.id}
+          views={video.views}
+          time={video.time}
+          title={video.title}
+          channel={video.channel}
+          verified={video.verified}
+        >
+          <PlayButton
+            onPlay={() => {
+              console.log("Playing", video.title);
+            }}
+            onPause={() => {
+              console.log("Paused", video.title);
+            }}
+          >
+            {video.title}
+          </PlayButton>
+        </Video>
+      ))}
+    </>
+  )
+}
+export default VideoList
