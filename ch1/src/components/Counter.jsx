c2c1599b7738b10d80648cdc94f378484ccca837
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 02:55:48 +0530

    File added

--- ch1/src/components/Counter.jsx
+++ ch1/src/components/Counter.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 function Counter() {
-  const [number,setNumber]=useState(0)
+  const [number, setNumber] = useState(0);
   function handleClickButton(event) {
-    event.stopPropagation()
-    setNumber(number+1)
-  } 
-  return(
-  <>
-    <h1 style={{color:'white'}}>{number}</h1>
-    <button onClick={handleClickButton}>Add</button>
-  </>)
+    event.stopPropagation();
+    setNumber(number + 1);
+  }
+  return (
+    <>
+      <h1 style={{ color: "white" }}>{number}</h1>
+      <button onClick={handleClickButton}>Add</button>
+    </>
+  );
 }
 export default Counter;
