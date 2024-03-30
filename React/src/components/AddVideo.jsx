747bed6da63f765eb9d0685dd4bd65bdd2096af0
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 04:30:19 +0530

    File added

--- React/src/components/AddVideo.jsx
+++ React/src/components/AddVideo.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import "./AddVideo.css";
-function AddVideo() {
-  const [video, setVideo] = useState({
+function AddVideo({ addVideos }) {
+  const [videos, setVideos] = useState({
     time: "1 year ago",
     channel: "Tridib",
     verified: true,
   });
   function handleSubmit(event) {
-    event.preventDefault()
-    console.log(video)
+    event.preventDefault();
+    addVideos();
+    console.log(videos);
   }
   function handleChange(e) {
     console.log(e.target.value);
-    setVideo({ ...video, [e.target.name]: e.target.value });
-    console.log(video)
+    setVideos({ ...videos, [e.target.name]: e.target.value });
+    console.log(videos);
   }
   return (
     <>
@@ -30,25 +31,7 @@ function AddVideo() {
           onChange={handleChange}
           placeholder="views"
         />
-        <button
-          onClick={handleSubmit}
-          // onClick={() => {
-          //   setVideos([
-          //     ...videos,
-          //     {
-          //       id: videos.length + 1,
-          //       url: "https://picsum.photos/id/1/160/90",
-          // views: "10K",
-          // time: "1 year ago",
-          // title: "Some random Tutorial",
-          // channel: "Tridib",
-          // verified: true,
-          //     },
-          //   ]);
-          // }}
-        >
-          Add Video
-        </button>
+        <button onClick={handleSubmit}>Add Video</button>
       </form>
     </>
   );
