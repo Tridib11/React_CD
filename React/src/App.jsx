1e6c11eb04bbb69097c14e8cb0f0aa376f07020f
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 05:02:49 +0530

    File added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import videoDB from "./data/data";
-import Video from "./components/Video";
 import React, { useState } from "react";
-import PlayButton from "./components/PlayButton";
-import Counter from "./components/Counter";
 import AddVideo from "./components/AddVideo";
+import VideoList from "./components/VideoList";
 
 function App() {
   const [videos, setVideos] = useState(videoDB);
@@ -17,28 +15,7 @@ function App() {
       <div className="Heading">
         <AddVideo addVideos={addVideos}></AddVideo>
       </div>
-      {videos.map((video) => (
-        <Video
-          key={video.id}
-          id={video.id}
-          views={video.views}
-          time={video.time}
-          title={video.title}
-          channel={video.channel}
-          verified={video.verified}
-        >
-          <PlayButton
-            onPlay={() => {
-              console.log("Playing", video.title);
-            }}
-            onPause={() => {
-              console.log("Paused", video.title);
-            }}
-          >
-            {video.title}
-          </PlayButton>
-        </Video>
-      ))}
+      <VideoList videos={videos}></VideoList>
     </div>
   );
 }
