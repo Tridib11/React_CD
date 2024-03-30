4cb9533727353c7bc43a01b1511d4acddea70c4b
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 04:04:02 +0530

    File added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,24 +4,15 @@ import Video from "./components/Video";
 import React, { useState } from "react";
 import PlayButton from "./components/PlayButton";
 import Counter from "./components/Counter";
+import AddVideo from "./components/AddVideo";
 
 function App() {
 
   const [videos,setVideos]=useState(videoDB)
   return (
-    <div className="App" onClick={()=>{console.log("App")}}>
+    <div className="App">
       <div className="Heading">
-        <button onClick={()=>{
-          setVideos([...videos,{
-            id: videos.length+ 1,
-            url: "https://picsum.photos/id/1/160/90",
-            views: "10K",
-            time: "1 year ago",
-            title: "Some random Tutorial",
-            channel: "Tridib",
-            verified: true,
-          }])
-        }}>Add Video</button>
+        <AddVideo></AddVideo>
       </div>
       {videos.map((video) => (
         <Video
