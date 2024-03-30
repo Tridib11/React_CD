13a37958a2f53788e65fbd9364e1b3088cc3d46c
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 02:02:56 +0530

    File modified

--- ch1/src/components/PlayButton.jsx
+++ ch1/src/components/PlayButton.jsx
@@ -2,7 +2,9 @@ import "./PlayButton.css";
 export default function PlayButton({ message, children, onPlay, onPause }) {
   // don't use this approach
   let playing = false;
-  function handleClick() {
+  function handleClick(event) {
+    // console.log(event)
+    event.stopPropagation()
     if (playing) onPause();
     else onPlay();
 
