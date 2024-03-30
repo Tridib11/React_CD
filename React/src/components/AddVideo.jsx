0c5290b940c7322ac6f12c1d3936e6fe1f7abf23
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 04:47:12 +0530

    File added

--- React/src/components/AddVideo.jsx
+++ React/src/components/AddVideo.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import "./AddVideo.css";
+
+const initialState={
+  time: "1 year ago",
+  channel: "Tridib",
+  verified: true,
+  title:"",
+  views:""
+}
 function AddVideo({ addVideos }) {
-  const [video, setVideos] = useState({
-    time: "1 year ago",
-    channel: "Tridib",
-    verified: true,
-    title:"",
-    views:""
-  });
+  const [video, setVideos] = useState(initialState);
   function handleSubmit(event) {
     event.preventDefault();
     addVideos(video);
+    setVideos(initialState)
   }
   function handleChange(e) {
     setVideos({ ...video, [e.target.name]: e.target.value });
