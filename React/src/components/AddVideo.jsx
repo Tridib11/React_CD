b9597c4a479e3cdc94940bc27fe80311b20028b2
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 04:05:53 +0530

    File added

--- React/src/components/AddVideo.jsx
+++ React/src/components/AddVideo.jsx
@@ -1,25 +1,32 @@
 import "./AddVideo.css"
 function AddVideo() {
+  function handleSubmit(){
+
+  }
+  function handleChange(){
+
+  }
   return (
     <>
       <form>
-        <input type="text" placeholder="title" />
-        <input type="text" placeholder="views"/>
+        <input type="text" onChange={handleChange} placeholder="title" />
+        <input type="text" onChange={handleChange} placeholder="views"/>
         <button
-          onClick={() => {
-            setVideos([
-              ...videos,
-              {
-                id: videos.length + 1,
-                url: "https://picsum.photos/id/1/160/90",
-                views: "10K",
-                time: "1 year ago",
-                title: "Some random Tutorial",
-                channel: "Tridib",
-                verified: true,
-              },
-            ]);
-          }}
+        onClick={handleSubmit}
+          // onClick={() => {
+          //   setVideos([
+          //     ...videos,
+          //     {
+          //       id: videos.length + 1,
+          //       url: "https://picsum.photos/id/1/160/90",
+          //       views: "10K",
+          //       time: "1 year ago",
+          //       title: "Some random Tutorial",
+          //       channel: "Tridib",
+          //       verified: true,
+          //     },
+          //   ]);
+          // }}
         >
           Add Video
         </button>
