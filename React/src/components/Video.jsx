bbc36d17c8ccfc30c8c6ac3e97b324b8a471127b
Tridib11 <tridibghosh12345@gmail.com>
2024-03-31 05:37:55 +0530

    File added

--- React/src/components/Video.jsx
+++ React/src/components/Video.jsx
@@ -1,25 +1,41 @@
-import "./Video.css"
-function Video({id, title, channel="default-name", views, time,verified=false,children,deleteVideo }) {
-
-  
+import "./Video.css";
+function Video({
+  id,
+  title,
+  channel = "default-name",
+  views,
+  time,
+  verified = false,
+  children,
+  deleteVideo,
+  editVideo,
+}) {
   return (
     <>
       <div className="container">
-        <button className="close" onClick={()=>deleteVideo(id)}>X</button>
+        <button className="close" onClick={() => deleteVideo(id)}>
+          X
+        </button>
+        <button className="edit" onClick={() => editVideo(id)}>
+          Edit
+        </button>
         <div className="pic">
-          <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
+          <img
+            src={`https://picsum.photos/id/${id}/160/90`}
+            alt="Katherine Johnson"
+          />
         </div>
         <div className="title">{title}</div>
-        <div className="channel">{channel} {verified ? '✅' : ''}</div>
+        <div className="channel">
+          {channel} {verified ? "✅" : ""}
+        </div>
 
         <div className="views">
-          {views } views <span>.</span> {time}
+          {views} views <span>.</span> {time}
         </div>
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     </>
   );
 }
-export default Video
+export default Video;
